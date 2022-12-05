7480edd73cd8d4b317e4bc5fd946f91abe79bc09
Betadrone <betadrone596@gmail.com>
2022-12-05 10:48:39 -0500

    updated but not perfected

--- function.js
+++ function.js
@@ -1,35 +1,43 @@
-var game = [{
-    id: 1,
-    Name: "Sonic the HedgeHog",
-    Platform: 'Switch',
-    Leaderboard:[{
-      id:1,
-      Player: "NATE",
-      Score: 10000,
-      Time: null
-    },{
-        id:2,
-        Player: "Muhammad",
-        Score: 10002,
-        Time: null
-      }]
-  },
-  {
-    id: 2,
-    Name: "Hollow Knight",
-    Platform: 'PC',
-    Leaderboard:[{
-      id:1,
-      Player: "Jayson",
-      Score: null,
-      Time: "20:21:56:02"
-    },{
-        id:2,
-        Player: "Dean",
-        Score: null,
-        Time: "19:22:40:20"
-      }]
-  }];
+// var game = [{
+//     id: 1,
+//     Name: "Sonic the HedgeHog",
+//     Platform: 'Switch',
+//     Leaderboard:[{
+//       id:1,
+//       Player: "NATE",
+//       Score: 10000,
+//       Time: null
+//     },{
+//         id:2,
+//         Player: "Muhammad",
+//         Score: 10002,
+//         Time: null
+//       }]
+//   },
+//   {
+//     id: 2,
+//     Name: "Hollow Knight",
+//     Platform: 'PC',
+//     Leaderboard:[{
+//       id:1,
+//       Player: "Jayson",
+//       Score: null,
+//       Time: "20:21:56:02"
+//     },{
+//         id:2,
+//         Player: "Dean",
+//         Score: null,
+//         Time: "19:22:40:20"
+//       }]
+//   }];
+
+fetch("https://lime-faithful-hippo.cyclic.app/api/games")
+.then(function(response){
+  return response.json()
+})
+.then(function(jsonObject){
+  console.log(jsonObject);
+})
 
   console.log("Yo")
 
@@ -40,6 +48,7 @@ var game = [{
     <th>Games</th> <th>Platform</th>\
     </tr>";
 
+    
     game.forEach(function(currentGame) {
         tableCode += `<tr><td>${currentGame.Name}</td>\
         <td>${currentGame.Platform}</td></tr>`;
@@ -51,17 +60,15 @@ var game = [{
     document.getElementById("gameTable").innerHTML = tableCode;
   };
 
-  // To move to the page that adds a new game:
-  function page2(){
-    
-  }
-
   // To add a new game along with the platform it's played on:
 function CreateGame(){
   let newGame;
-  newGame = document.getElementById("gameName").value;
+  newGame = document.getElementById("gameName").value.trim();
   let platform;
-  platform = document.getElementById("platformName").value;
+  platform = document.getElementById("platformName").value.trim();
+  let clear = "";
+  document.getElementById("gameName").value = clear;
+  document.getElementById("platformName").value = clear;
   console.log(newGame);
   console.log(platform);
 }
